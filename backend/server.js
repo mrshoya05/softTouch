40acb2ca5a4bcdcf04401d81dbdc7375d0a43bed
mrshoya05 <shoyabkhan0508@gmail.com>
2025-01-20 12:00:44 +0530

    connect repo in another sys

--- backend/server.js
+++ backend/server.js
@@ -31,5 +31,3 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//mantaining streak  
-// again mantaing for streak 

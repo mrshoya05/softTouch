54b588909fc77dd6b0733d6763560fdeaa7eec71
mrshoya05 <shoyabkhan0508@gmail.com>
2025-01-21 12:05:07 +0530

    frontend

--- frontend/src/utils/ProtectedRoutes.jsx
+++ frontend/src/utils/ProtectedRoutes.jsx
@@ -1,8 +1,9 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/" />;
+  // const token = localStorage.getItem("token");
+  const  role = localStorage.getItem("role");
+  return role == "Admin" ? children : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;

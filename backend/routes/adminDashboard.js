7d774a10e158254585b4af423c6a57f0f46fe9b4
mrshoya05 <shoyabkhan0508@gmail.com>
2025-01-22 14:49:12 +0530

    feat: Add deleteUser functionality to admin dashboard
    This commit adds the deleteUser functionality to the admin dashboard. It includes a new controller function in the fetchUsers module to handle the deletion of a user. The deleteUser function retrieves the user by ID, removes it from the database, and sends a success message back to the client. If any errors occur during the deletion process, an appropriate error message is returned.

--- backend/routes/adminDashboard.js
+++ backend/routes/adminDashboard.js
@@ -4,9 +4,12 @@ const express = require("express");
 const router = express.Router();
 
 const adminAuth = require("../middlewares/adminAuth");
+const { fetchUsers, deleteUser } = require("../controllers/admin/fetchUsers");
 
-const  fetchUsers  = require("../controllers/admin/fetchUsers");
+// const deleteUser = require("../controllers/admin");
 
 router.get("/getusers", adminAuth, fetchUsers);
+// delete 
+router.delete("/user/delete/:id", adminAuth, deleteUser);
 
 module.exports = router;
